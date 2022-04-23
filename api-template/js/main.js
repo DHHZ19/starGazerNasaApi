b964bbd94d0b74bbb3919c272a8f1f3d7fea397d
Diego Hernandez <62413339+DHHZ19@users.noreply.github.com>
2022-04-22 20:46:39 -0500

    A commit containing the beginning of a great proj

---
+++ api-template/js/main.js
@@ -0,0 +1,29 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+// title: "Planet Earth at Twilight"
+  const url = `https://api.nasa.gov/planetary/apod?api_key=zU71SV2z8UAS2tpSRxtx9Ii4giGUAk6QIufK4bCn&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if( data.media_type === 'image' ){
+          document.querySelector('img').src = data.hdurl
+          document.querySelector('h2').innerText = data.title
+          document.querySelector('.date').innerHTML = data.date
+        }else if(data.media_type === 'video'){
+          document.querySelector('iframe').src = data.url
+          document.querySelector('h2').innerText = data.title
+        }
+       
+        document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
